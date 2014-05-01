3690d1aa318fa22bdb9ef472d1ab30e64a431431
PolarNick <PolarHare@gmail.com>
2014-05-02 02:37:25 +0400

    Project created for javascript tutorial bundle.

---
+++ src/main/java/org/codetrials/examples/javascript/Bundle.java
@@ -0,0 +1,9 @@
+package org.codetrials.examples.javascript;
+
+import org.codetrials.bundle.BundleContainer;
+
+/**
+ * @author Polyarnyi Nikolay
+ */
+public class Bundle extends BundleContainer {
+}
