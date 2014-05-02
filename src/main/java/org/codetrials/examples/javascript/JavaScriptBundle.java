111859a434074418d3315219ccd17ae3bbf38773
vlpolyansky <vl-polyansky@yandex.ru>
2014-05-02 10:55:17 +0400

    bundle update

--- src/main/java/org/codetrials/examples/javascript/JavaScriptBundle.java
+++ src/main/java/org/codetrials/examples/javascript/JavaScriptBundle.java
@@ -12,6 +12,7 @@ import org.codetrials.bundle.helpers.tasks.SingleRegexpTask;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 /**
  * @author Polyarnyi Nikolay
@@ -45,21 +46,22 @@ public class JavaScriptBundle extends SimpleBundleContainer {
 
                 @Override
                 public TaskReaction onCommandExecuted(String s, ExecutionResult executionResult) {
-                    flag = executionResult.getException() == null;
+                    flag = executionResult != null && executionResult.getException() == null;
                     return new TaskReaction("");
                 }
             };
             Task task1 = new SingleRegexpTask(new TaskDescription("Summary", "Show us your skills, " +
-                    "in summing two numbers"), ".*\\\\+.*");
-            Task task2 = new MultipleRegexpTask(loader.loadTaskDescription(2), null, "next", "\".*\"");
-            ArrayList<Task> list = new ArrayList<Task>();
+                    "in summing two numbers"), ".*\\+.*");
+            Task task2 = new MultipleRegexpTask(loader.loadTaskDescription(2), "write string, mafaka",
+                    "next", "\\s*\".*\"\\s*");
+            ArrayList<Task> list = new ArrayList<>();
             list.add(task0);
             list.add(task1);
             list.add(task2);
             return list;
         } catch (IOException ex) {
             ex.printStackTrace();
-            return new ArrayList<Task>();
+            return new ArrayList<>();
         }
     }
 
@@ -81,6 +83,7 @@ public class JavaScriptBundle extends SimpleBundleContainer {
 
     public static void main(String[] args) {
         JavaScriptBundle jsb = new JavaScriptBundle();
-
+        Pattern p = Pattern.compile("\\s*\".*\"\\s*");
+        System.out.println(p.matcher(" \" abdf \"   ").matches());
     }
 }
