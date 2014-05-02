16542ec1d38f0ac718b22c23ead0772f66b03be6
vlpolyansky <vl-polyansky@yandex.ru>
2014-05-02 19:48:17 +0400

    changing bundle

--- src/main/java/org/codetrials/examples/javascript/JavaScriptBundle.java
+++ src/main/java/org/codetrials/examples/javascript/JavaScriptBundle.java
@@ -3,17 +3,14 @@ package org.codetrials.examples.javascript;
 import org.codetrials.bundle.Task;
 import org.codetrials.bundle.engines.BundleEngine;
 import org.codetrials.bundle.engines.JavaScriptEngine;
-import org.codetrials.bundle.entities.ExecutionResult;
 import org.codetrials.bundle.entities.TaskDescription;
-import org.codetrials.bundle.entities.TaskReaction;
 import org.codetrials.bundle.helpers.SimpleBundleContainer;
+import org.codetrials.bundle.helpers.tasks.FreeTask;
 import org.codetrials.bundle.helpers.tasks.MultipleRegexpTask;
-import org.codetrials.bundle.helpers.tasks.SingleRegexpTask;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Pattern;
 
 /**
  * @author Polyarnyi Nikolay
@@ -27,37 +24,29 @@ public class JavaScriptBundle extends SimpleBundleContainer {
         super(new JavaScriptEngine());
     }
 
+    private TaskDescription t(int id) throws IOException {
+        return resourceLoader.loadTaskDescription(id);
+    }
+
     @Override
     protected List<Task> createTasks() {
         try {
-            Task task0 = new Task(new TaskDescription("Test slide", "Do some compilable shit")) {
-
-                private boolean flag = false;
-
-                @Override
-                public boolean isCompleted() {
-                    return flag;
-                }
-
-                @Override
-                public boolean isCommandExecutable(String s) {
-                    return true;
-                }
-
-                @Override
-                public TaskReaction onCommandExecuted(String s, ExecutionResult executionResult) {
-                    flag = executionResult != null && executionResult.getException() == null;
-                    return new TaskReaction("");
-                }
-            };
-            Task task1 = new SingleRegexpTask(new TaskDescription("Summary", "Show us your skills, " +
-                    "in summing two numbers"), ".*\\+.*");
-            Task task2 = new MultipleRegexpTask(resourceLoader.loadTaskDescription(2), "write string, mafaka",
-                    "next", "\\s*\".*\"\\s*");
+            Task task1 = new FreeTask(t(1), "next");
+            Task task2 = new FreeTask(t(2), "next");
+            Task task3 = new FreeTask(t(3), "next");
+            Task task4 = new FreeTask(t(4), "next");
+            Task task5 = new MultipleRegexpTask(t(5), "Try using arithmetic operations", "next",
+                    ".*\\+.*", ".*\\*.*", ".*-.*");
+            Task task6 = new FreeTask(t(4), "next");
+            Task task7 = new FreeTask(t(4), "next");
             ArrayList<Task> list = new ArrayList<>();
-            list.add(task0);
             list.add(task1);
             list.add(task2);
+            list.add(task3);
+            list.add(task4);
+            list.add(task5);
+            list.add(task6);
+            list.add(task7);
             return list;
         } catch (IOException ex) {
             ex.printStackTrace();
@@ -76,9 +65,4 @@ public class JavaScriptBundle extends SimpleBundleContainer {
                 "From simple variables and math to some interesting things!";
     }
 
-    public static void main(String[] args) {
-        JavaScriptBundle jsb = new JavaScriptBundle();
-        Pattern p = Pattern.compile("\\s*\".*\"\\s*");
-        System.out.println(p.matcher(" \" abdf \"   ").matches());
-    }
 }
